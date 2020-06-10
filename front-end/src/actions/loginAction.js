5095926ed987e123d606e19e0da2ed7fb0121f44
hoang <vinhyenvodoi98@gmail.com>
2020-06-10 14:03:50 +0700

    feat : setup redirect by role

--- front-end/src/actions/loginAction.js
+++ front-end/src/actions/loginAction.js
@@ -14,6 +14,7 @@ export const login = (token) => async (dispatch) => {
         type: LOGIN,
         isLogin: true,
         name: response.data.name,
+        role: response.data.role,
         token
       });
       dispatch({
--- front-end/src/views/Home/index.js
+++ front-end/src/views/Home/index.js
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import * as UI from 'actions/UIAction';
 import Carou from 'components/Carousel';
 import ListCourse from 'components/ListCourse';
 
 function Home() {
   const dispatch = useDispatch();
+  const LoginStatus = useSelector((state) => state.LoginStatus);
 
   useEffect(() => {
     dispatch(UI.updateMenuKey(1));
@@ -13,6 +15,16 @@ function Home() {
 
   return (
     <div>
+      {/* TODO false is admin */}
+      {LoginStatus.role ? (
+        <></>
+      ) : (
+        <Redirect
+          to={{
+            pathname: '/admin'
+          }}
+        />
+      )}
       <Carou />
       <ListCourse />
     </div>
