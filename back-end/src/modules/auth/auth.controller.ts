0a539b4d56814cd7569533d2cf48cd33610fe55d
duongle26 <leduong26298@gmail.com>
2020-06-20 16:29:43 +0700

    fix: field summary and field auth in swagger

--- back-end/src/modules/auth/auth.controller.ts
+++ back-end/src/modules/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
-import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { ApiTags, ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { SignUpDto, SignInDto } from './auth.dto';
@@ -11,16 +11,19 @@ import { JwtPayload } from './jwt-payload.model';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Sign Up' })
   @Post('/signup')
   signUp(@Body() signUpDto: SignUpDto): Promise<void> {
     return this.authService.signUp(signUpDto);
   }
 
+  @ApiOperation({ summary: 'Sign In' })
   @Post('/signin')
   signIn(@Body() signInDto: SignInDto): Promise<{ accessToken: string }> {
     return this.authService.signIn(signInDto);
   }
 
+  @ApiOperation({ summary: 'Verify Token' })
   @ApiBearerAuth()
   @UseGuards(AuthGuard())
   @Get('verify')
