eb4134a39cdbb0219f6c7b7463aeb812cc34bed0
duongle26 <leduong26298@gmail.com>
2020-06-20 16:33:59 +0700

    feat: filter out all properties that are not defined in dto

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('/api/v1');
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
 
   const options = new DocumentBuilder().setTitle('minano-betonamugo API v1').addBearerAuth().build();
   const document = SwaggerModule.createDocument(app, options);
--- back-end/src/modules/users/users.dto.ts
+++ back-end/src/modules/users/users.dto.ts
@@ -21,7 +21,4 @@ export class UpdateUserDto {
   @IsOptional()
   @IsBoolean()
   isBlock: boolean;
-
-  @IsEmpty()
-  role: boolean;
 }
