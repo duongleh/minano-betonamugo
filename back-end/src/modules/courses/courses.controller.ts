896ec788f8ea14c91c26e58088c7c1bd1b73796a
duongle26 <leduong26298@gmail.com>
2020-06-23 18:44:13 +0700

    feat: add enrollments and videos to get course api

--- back-end/src/modules/courses/courses.controller.ts
+++ back-end/src/modules/courses/courses.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, UseGuards } from '@nestjs/common';
 import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
-import { Crud, CrudController, Override, ParsedRequest, ParsedBody, CrudRequest } from '@nestjsx/crud';
+import { Crud, CrudController, Override, ParsedRequest, ParsedBody, CrudRequest, CrudRequestInterceptor } from '@nestjsx/crud';
 import { Course } from './courses.entity';
 import { CoursesService } from './courses.service';
 import { CreateCourseDto, UpdateCourseDto } from './courses.dto';
@@ -16,6 +16,13 @@ import { RolesGuard } from '../../shared/Guards/roles.guard';
     exclude: ['createManyBase', 'replaceOneBase'],
     updateOneBase: { decorators: [ApiBearerAuth(), UseGuards(AuthGuard(), RolesGuard)] },
     deleteOneBase: { decorators: [ApiBearerAuth(), UseGuards(AuthGuard(), RolesGuard)] }
+  },
+  query: {
+    join: {
+      videos: { exclude: ['courseId', 'url'] },
+      enrollments: { exclude: ['courseId'] },
+      'enrollments.user': {}
+    }
   },
   dto: {
     create: CreateCourseDto,
